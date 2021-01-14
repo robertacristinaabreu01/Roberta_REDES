53f3be3b519e4b932cd72f52249548f0bc966f22
Roberta Abreu <76497778+robertacristinaabreu01@users.noreply.github.com>
2021-01-14 17:46:57 -0300

    Add files via upload

---
+++ Capitulo4_Dicionarios/Dicionarios1.py
@@ -0,0 +1,13 @@
+usuarios={}
+usuarios={
+    "Chaves":["Chaves Silva","17/06/2017","Recep_01"],
+    "Quico":["Enrico Flores","03/06/2017","Raiox_02"]
+    }
+
+##incluir itens no dicionário
+#usuarios["Florinda"]=["Florinda Flores", "26/11/2017", "Recep_01"]
+#usuarios["Florinda"]=["Florinda Flores", "26/11/2016", "Recep_01"]
+
+print(usuarios)
+print("##############========#########")
+print("Dados: ",usuarios.get("Chaves"))
